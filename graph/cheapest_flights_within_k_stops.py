b0669fb1b113937d2513ece1540b2868e997fead
lanesky <shangkeyun@gmail.com>
2021-09-17 08:04:33 +0900

    bellman ford

---
+++ graph/cheapest_flights_within_k_stops.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/cheapest-flights-within-k-stops/
+# Time O(k*n^2) | Space O(n)
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+        
+        distance = [float('inf')] * n
+        distance[src] = 0
+        
+        # Run Bellman-Ford
+        for _ in range(k+1):            
+            curr = distance[:]           
+            for i,j,price in flights:
+                if distance[i] + price < curr[j]:
+                    curr[j] = distance[i] + price 
+            print(distance)
+            distance = curr
+                                        
+        return distance[dst] if distance[dst] != float('inf') else -1
